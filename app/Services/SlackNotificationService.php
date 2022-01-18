64a5af53b1aeaf03aa2e883f580de3c733a4eb01
Yu Suzuki <haraheri7212@yahoo.co.jp>
2022-01-18 09:06:25 +0900

    add Slack notification

---
+++ app/Services/SlackNotificationService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Services;
+use App\Notifications\SlackNotification;
+use Illuminate\Notifications\Notifiable;
+class SlackNotificationService
+{
+    use Notifiable;
+
+    public function send($message)
+    {
+        $this->notify(new SlackNotification($message));
+    }
+    /**
+     * Slackチャンネルに対する通知をルートする
+     *
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return string
+     */
+    public function routeNotificationForSlack($notification)
+    {
+        return env('SLACK_URL');
+    }
+}
